6e691aaa3f519f8769d8fc46c768262df521f4ce
Soroush Nejad <soroush.nejad@kits.se>
2016-05-17 16:51:25 +0200

    ADD: Convert a youtube url to peggy to make it easy to download the video

---
+++ ConvertYoutubeUrlToPeggyUrl.py
@@ -0,0 +1,36 @@
+import sys
+import re
+import subprocess
+import webbrowser
+
+
+def check(youtube):
+    print "Hej again"
+    yt_id = youtube.split('https://www.youtube.com/watch?v=')[1].split('&')[0]
+    peggo_url = 'http://peggo.co/dvr/' + yt_id + '#rv'
+    write_to_clipboard(peggo_url)
+    webbrowser.open(peggo_url)
+    print peggo_url
+    # match = re.search(r'https://www.youtube.com/watch\?v=', youtube)
+    # if match:
+    #     found = match.group()
+    #     print found
+    # else:
+    #     print 'Not found''
+
+def write_to_clipboard(output):
+    process = subprocess.Popen(
+        'pbcopy', env={'LANG': 'en_US.UTF-8'}, stdin=subprocess.PIPE)
+    process.communicate(output.encode('utf-8'))
+
+if __name__ == '__main__':
+    if len(sys.argv) != 2:
+        print 'usage ./MakeUrl.py INPUT'
+        sys.exit(1)
+    check(sys.argv[1])
+
+
+def my_lm(s):
+    return s[2]
+
+l.sort(lambda=my_lm)
